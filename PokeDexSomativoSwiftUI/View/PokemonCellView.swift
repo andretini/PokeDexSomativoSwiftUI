76c69d9fe4a358191ec34d9f247e7e327c84b45b
Rafael Galafassi <93929600+Tagliassi@users.noreply.github.com>
2025-06-16 23:40:41 -0300

    Update PokemonCellView.swift

--- PokeDexSomativoSwiftUI/View/PokemonCellView.swift
+++ PokeDexSomativoSwiftUI/View/PokemonCellView.swift
@@ -4,21 +4,23 @@
 //
 //  Created by user277041 on 6/14/25.
 //
-import SwiftUI;
+import SwiftUI
 
 struct PokemonCellView: View {
     let imageUrl: String?
     let pokemon: NamedAPIResource
+    let animation: Namespace.ID
 
     var body: some View {
         VStack {
             PokemonImageView(pokemonUrl: pokemon.url, image: imageUrl)
+                .matchedGeometryEffect(id: pokemon.name, in: animation)
             Text(pokemon.name.capitalized)
-                .font(.headline)
-                .foregroundColor(.primary)
+                .font(AppFont.title.font())
+                .foregroundColor(AppColor.textPrimary.color)
         }
         .frame(maxWidth: .infinity, minHeight: 80)
-        .background(Color.green.opacity(0.2))
-        .cornerRadius(8)
+        .background(AppColor.cellBackground.color)
+        .cornerRadius(AppCornerRadius.medium.rawValue)
     }
 }
